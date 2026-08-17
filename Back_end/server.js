5c91a92b00fd8bd6ff687767698a8702355a2d5e
burlee2004 <burlee2004@gmail.com>
2026-08-17 22:52:21 +0700

    fix: Syntax error in server.js

--- Back_end/server.js
+++ Back_end/server.js
@@ -470,7 +470,7 @@ app.post('/api/milestones/advanced/submit', async (req, res) => {
             .from('milestones')
             .update({ 
                 proof_urls: proof_urls, 
-                evidence_url: report_text, -- Tạm dùng trường cũ lưu text báo cáo
+                evidence_url: report_text, // Tạm dùng trường cũ lưu text báo cáo
                 status: 'PENDING_REVIEW' 
             })
             .eq('id', milestone_id)
